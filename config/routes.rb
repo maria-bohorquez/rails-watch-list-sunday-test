8419eae62b5fc9cc635e70531be27ad122d44cd1
Maria Bohorquez <a.maria.bohorquez@gmail.com>
2021-07-27 15:08:43 +0200

    models and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'lists', to: 'lists#index'
+  get 'lists/new', to: 'lists#new', as: :new_list
+  post 'lists', to: 'lists#create'
 end
